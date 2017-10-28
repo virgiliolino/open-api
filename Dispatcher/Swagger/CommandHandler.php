ad836318fa47ba638cad6e1ff5242b5226f0d114
Virgilio Lino <virgilio@riskident.com>
2017-10-28 20:52:40 +0200

    Addition of commandHandler interface

--- Dispatcher/Swagger/CommandHandler.php
+++ Dispatcher/Swagger/CommandHandler.php
@@ -1,8 +1,13 @@
 <?php
+namespace Dispatcher\Swagger;
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+interface CommandHandler {
+
+    public static function execute(
+        \Slim\Http\Request $request,
+        \Slim\Http\Response $response,
+        $params
+    );
+
+}
 
