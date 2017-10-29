39a1a48629ecfbc549cf7f42ff488625aee272bd
Virgilio Lino <virgiliolino@gmail.com>
2017-10-29 18:27:01 +0100

    removal of commented code

--- Examples/HelloWorld/public/index.php
+++ Examples/HelloWorld/public/index.php
@@ -5,11 +5,4 @@ $swaggerApiFile = 'routes.json';
 $swaggerConfigParser = Dispatcher\Swagger\ParserFactory::parserFor($swaggerApiFile);
 $swaggerConfig = $swaggerConfigParser->parse($swaggerApiFile);
 \Dispatcher\Swagger\SwaggerDispatcher::InjectRoutesFromConfig($app, $swaggerConfig); 
-/*$app->get('/hello/{name}', function (Request $request, Response $response) {
-	    $name = $request->getAttribute('name');
-	        $response->getBody()->write("Hello, $name");
-
-	        return $response;
-});*/
-
 $app->run();
