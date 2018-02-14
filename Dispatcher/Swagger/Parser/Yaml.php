388841b5b1bbecc938723e173ebbfee344774576
Virgilio Lino <virgiliolino@gmail.com>
2018-02-14 14:32:32 +0100

    remotion of unused file

--- Dispatcher/Swagger/Parser/Yaml.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Dispatcher\Swagger\Parser;
-
-class Yaml implements \Dispatcher\Swagger\Parser {
-    public function parse($path): array {
-        return \yaml_parse_file($path);
-    }
-}
